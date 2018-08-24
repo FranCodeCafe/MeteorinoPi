151eb4eaff7a540c4a5ea075ce9d7d6884d7cf61
Fran Van Houten <42654223+FranVanHouten@users.noreply.github.com>
2018-08-23 21:16:30 -0300

    Add files via upload

--- meteorino.sql
+++ meteorino.sql
@@ -1,9 +1,9 @@
-﻿-- phpMyAdmin SQL Dump
+-- phpMyAdmin SQL Dump
 -- version 4.7.4
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 23-08-2018 a las 21:20:26
+-- Tiempo de generación: 24-08-2018 a las 02:15:32
 -- Versión del servidor: 10.1.29-MariaDB
 -- Versión de PHP: 7.2.0
 
@@ -29,7 +29,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `gps` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `fecha` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `longitud` float DEFAULT NULL,
   `latitud` float DEFAULT NULL,
@@ -43,20 +43,28 @@ CREATE TABLE `gps` (
 --
 
 CREATE TABLE `login` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `nivel` int(1) NOT NULL,
   `usuario` varchar(20) NOT NULL,
   `email` varchar(50) NOT NULL,
   `clave` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
---------------------------------------------------------
+
+--
+-- Volcado de datos para la tabla `login`
+--
+
+INSERT INTO `login` (`id`, `nivel`, `usuario`, `email`, `clave`) VALUES
+(1, 1, 'superadmin', 'franciscasalinascastillo@gmail.com', 'superadmin');
+
+-- --------------------------------------------------------
 
 --
 -- Estructura de tabla para la tabla `weather`
 --
 
 CREATE TABLE `weather` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `fecha` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `hora` time DEFAULT NULL,
   `humedad` float DEFAULT NULL,
@@ -71,3 +79,52 @@ CREATE TABLE `weather` (
   `polvo` float DEFAULT NULL,
   `temperatura2` float DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `gps`
+--
+ALTER TABLE `gps`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `weather`
+--
+ALTER TABLE `weather`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `gps`
+--
+ALTER TABLE `gps`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `login`
+--
+ALTER TABLE `login`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `weather`
+--
+ALTER TABLE `weather`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
