01a632e42dc7f592b8273b09e2e8b973958fb3f9
Fran Van Houten <42654223+FranVanHouten@users.noreply.github.com>
2019-03-13 13:03:56 -0300

    Create mailtest.php

---
+++ meteorino/phpmailer/mailtest.php
@@ -0,0 +1,36 @@
+<?php
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+require 'vendor/autoload.php';
+
+$mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+try {
+    //Server settings
+    $mail->SMTPDebug = 2;                                 // Enable verbose debug output
+    $mail->isSMTP();                                      // Set mailer to use SMTP
+    $mail->Host = 'smtp.gmail.com'; 
+	$mail->SMTPAuth = true;                            
+	$mail->Username = 'meteorinopi@gmail.com';        
+	$mail->Password = 'meteorinopi2018';                   
+	$mail->SMTPSecure = 'tls';                       
+	$mail->Port = 587;                                     // TCP port to connect to
+
+    //Recipients
+    $mail->setFrom('meteorinopi@gmail.com', 'MeteorinoPi');
+	$mail->addAddress('franciscasalinascastillo@gmail.com');
+
+    //Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
+}
