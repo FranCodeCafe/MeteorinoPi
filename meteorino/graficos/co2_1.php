bea90b34ad2c7455bf9ec7f22d979c33e7286ae8
Fran Van Houten <42654223+FranVanHouten@users.noreply.github.com>
2019-03-13 12:56:20 -0300

    Create co2_1.php

---
+++ meteorino/graficos/co2_1.php
@@ -0,0 +1,70 @@
+    <!-- //////////////////// LOGIN FORM /////////////////////////// -->
+    <div class="modal fade" id="co2_1" role="dialog">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title"><i class="fas fa-tint"></i> Dióxido de carbono en la última hora</h4>
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                </div>
+                <div class="modal-body">
+                <?php 
+                require('conexion.php');
+                    $sql = "SELECT * from `weather` ORDER BY `id` DESC LIMIT 70";
+                    $reg = mysqli_query($con,$sql);
+                    if ($reg->num_rows > 0) {
+                        while($row = mysqli_fetch_array($reg)){
+                                $co2 = $row['co2'];
+                                $fecha = strtotime($row['fecha2'])*1000;
+                                $co221[] = "[$fecha,$co2]";
+                        }
+                    }
+                            
+                ?>
+
+                    <div id="cont-co2-1" style="min-width: 310px; height: 400px; margin: 0 auto"></div>
+                    <script src="code/highcharts.js"></script>
+                    <script src="code/modules/exporting.js"></script>
+                    <script src="code/modules/export-data.js"></script>
+                    <script type="text/javascript">
+                        Highcharts.chart('cont-co2-1', {
+                            chart: {
+                                type: 'area'
+                            },
+                            title: {
+                                text: 'Dióxido de carbono en la última hora'
+                            },
+                            subtitle: {
+                                text: 'MeteorinoPi'
+                            },
+                            xAxis: {
+                                type: 'datetime',
+                                labels: {
+                                    format: '{value:%e-%m-%Y %H:%M}',
+                                    rotation: -60
+                                }
+                            },
+                            yAxis: {
+                                title: {
+                                    text: '%'
+                                }
+                            },
+                            plotOptions: {
+                                area: {
+                                    dataLabels: {
+                                        enabled: true
+                                    },
+                                    enableMouseTracking: true
+                                }
+                            },
+                            series: [{
+                                data: [<?php echo join($co221, ','); ?>],
+                                name: 'Dióxido de carbono en el aire',
+                                color: '#757575'
+                            }]
+                        });
+                    </script>     
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--//////////////////////////////////////////////////////-->
